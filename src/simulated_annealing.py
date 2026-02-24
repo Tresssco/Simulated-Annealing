439bdfb6ce2c60453ef3f3dab01bdf6b41348545
Tresssco <jorge.escolano.gon@gmail.com>
2026-02-24 13:29:36 +0100

    feat(src): :sparkles: añadir conjuntos radios y estados
    se genera nuestro espacio de estados y las estaciones de radio y sus coberturas

---
+++ src/simulated_annealing.py
@@ -0,0 +1,25 @@
+import random,math
+
+# USA states where we want to be listened
+needed_states = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+added_states = set(
+    ["nm", "tx", "ok", "ks", "co", "ne", "sd", "wy", "nd", "ia", "mn", "mo", "ar", "la"]
+)
+
+needed_states.update(added_states)
+
+# Radio stations and states covereds
+stations = {}
+stations["kone"] = set(["id", "nv", "ut"])
+stations["ktwo"] = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfour"] = set(["nv", "ut"])
+stations["kfive"] = set(["ca", "az"])
+stations["ksix"] = set(["nm", "tx", "ok"])
+stations["kseven"] = set(["ok", "ks", "co"])
+stations["keight"] = set(["ks", "co", "ne"])
+stations["knine"] = set(["ne", "sd", "wy"])
+stations["kten"] = set(["nd", "ia"])
+stations["keleven"] = set(["mn", "mo", "ar"])
+stations["ktwelve"] = set(["la"])
+stations["kthirteen"] = set(["mo", "ar"])
